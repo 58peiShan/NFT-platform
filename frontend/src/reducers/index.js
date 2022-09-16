646323bde20768e2b935afd15d160b8493bff40b
Penny <penny.wu@incrte.com>
2022-09-16 13:27:45 +0800

    #1 1.測試

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from "redux";
 import productReducer from "./productReducer";
 import cartlist from "./cartlistReducer";
 import cardBtn from "./cardBtnReducer";
-import signUp from "./userSignupMsgReducer";
 
 
 export default combineReducers({
   productReducer,
   cartlist,
   cardBtn,
-  signUp,
 });
